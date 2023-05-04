75208f31baf7f148a328ed374bd1c7134ba42917
Flavio <fhnmor21@gmail.com>
2023-05-04 20:11:29 +1000

    merged options changes from upstream with local improvement to mappings and support to sessions

--- lua/config/func_utils.lua
+++ lua/config/func_utils.lua
@@ -1,3 +1,5 @@
+local Util = require("lazy.core.util")
+
 local M = {}
 
 -- returns the root directory based on:
@@ -59,6 +61,39 @@ function M.on_attach(on_attach)
   })
 end
 
+---@param silent boolean?
+---@param values? {[1]:any, [2]:any}
+function M.toggle(option, silent, values)
+  if values then
+    if vim.opt_local[option]:get() == values[1] then
+      vim.opt_local[option] = values[2]
+    else
+      vim.opt_local[option] = values[1]
+    end
+    return Util.info("Set " .. option .. " to " .. vim.opt_local[option]:get(), { title = "Option" })
+  end
+  vim.opt_local[option] = not vim.opt_local[option]:get()
+  if not silent then
+    if vim.opt_local[option]:get() then
+      Util.info("Enabled " .. option, { title = "Option" })
+    else
+      Util.warn("Disabled " .. option, { title = "Option" })
+    end
+  end
+end
+
+local enabled = true
+function M.toggle_diagnostics()
+  enabled = not enabled
+  if enabled then
+    vim.diagnostic.enable()
+    Util.info("Enabled diagnostics", { title = "Diagnostics" })
+  else
+    vim.diagnostic.disable()
+    Util.warn("Disabled diagnostics", { title = "Diagnostics" })
+  end
+end
+
 ---@param name string
 function M.opts(name)
   local plugin = require("lazy.core.config").plugins[name]
@@ -109,6 +144,19 @@ function M.telescope(builtin, opts)
       end
     end
     require("telescope.builtin")[builtin](opts)
+  end
+end
+
+-- Opens a floating terminal (interactive by default)
+---@param cmd? string[]|string
+---@param opts? LazyCmdOptions|{interactive?:boolean, esc_esc?:false}
+function M.float_term(cmd, opts)
+  opts = vim.tbl_deep_extend("force", {
+    size = { width = 0.9, height = 0.9 },
+  }, opts or {})
+  local float = require("lazy.util").float_term(cmd, opts)
+  if opts.esc_esc == false then
+    vim.keymap.set("t", "<esc>", "<esc>", { buffer = float.buf, nowait = true })
   end
 end
 
