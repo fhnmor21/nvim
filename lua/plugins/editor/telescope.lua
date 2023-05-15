62f6fe07c48839c13576b7db85360c1e50d49782
Flavio Moreira <fhnmor21@gmail.com>
2023-05-16 09:37:08 +1000

    change telescope fzf build command

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -13,7 +13,7 @@ return {
         "nvim-telescope/telescope-fzf-native.nvim",
         -- NOTE: If you are having trouble with this installation,
         --       refer to the README for telescope-fzf-native for more instructions.
-        build = "make",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
         cond = function()
           return vim.fn.executable("make") == 1
         end,
