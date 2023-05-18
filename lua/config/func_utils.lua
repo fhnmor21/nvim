14ddb10b9f42206265e9f2bacf49e6f4f04039db
cypherfoundry <cypherfoundry@protonmail.com>
2023-05-18 20:53:31 +1000

    cleaning up func_utils

--- lua/config/func_utils.lua
+++ lua/config/func_utils.lua
@@ -15,9 +15,12 @@ function M.get_root()
   if path then
     for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
       local workspace = client.config.workspace_folders
-      local paths = workspace and vim.tbl_map(function(ws)
-        return vim.uri_to_fname(ws.uri)
-      end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+      local paths = workspace and vim.tbl_map(
+            function(ws)
+              return vim.uri_to_fname(ws.uri)
+            end,
+            workspace) or
+          client.config.root_dir and { client.config.root_dir } or {}
       for _, p in ipairs(paths) do
         local r = vim.loop.fs_realpath(p)
         if path:find(r, 1, true) then
@@ -26,9 +29,11 @@ function M.get_root()
       end
     end
   end
-  table.sort(roots, function(a, b)
-    return #a > #b
-  end)
+
+  table.sort(roots,
+    function(a, b)
+      return #a > #b
+    end)
   local root = roots[1]
   if not root then
     path = path and vim.fs.dirname(path) or vim.loop.cwd()
