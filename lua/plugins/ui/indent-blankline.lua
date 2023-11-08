5009c40d58ece19e348e70338227b9828bfaa5f7
Flavio Moreira <fhnmor21@gmail.com>
2023-11-09 03:56:50 +1100

    updating and fixing ident-blankline vonfig to v3

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -1,11 +1,12 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   event = { "BufReadPost", "BufNewFile" },
   opts = {
     -- char = "▏",
-    char = "│",
-    filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-    show_trailing_blankline_indent = false,
-    show_current_context = false,
+    -- char = "│",
+    -- filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+    -- show_trailing_blankline_indent = false,
+    -- show_current_context = false,
   },
 }
