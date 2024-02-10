309905c416cabaab83afee8edb9a08f0d4688d41
Flavio <fhnmor21@gmail.com>
2024-02-10 22:47:33 +1100

    adding neotree

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -1,5 +1,14 @@
 return {
+  -- icons
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
+  -- ui components
+  { "MunifTanjim/nui.nvim",        lazy = true },
+
   -- the colorscheme should be available when starting Neovim
   require("plugins.ui.colortheme"),
+
+   -- file explorer
+   require("plugins.ui.neotree"),
 
 }
