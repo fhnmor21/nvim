0dc32dc457674b263100e8ffd4bdb317f98dbe0e
Flavio <fhnmor21@gmail.com>
2024-02-13 10:19:11 +1100

    added search, magic jump, illuminate, and multi select to the editor

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -5,4 +5,20 @@ return {
   -- fuzzy finder
   require("plugins.editor.telescope"),
 
+  -- magic jump within the buffer
+  require("plugins.editor.jump"),
+
+  -- references
+  require("plugins.editor.illuminate"),
+
+  -- search/replace in multiple files
+  require("plugins.editor.spectre"),
+
+  -- cursor multi-select
+  {
+    'mg979/vim-visual-multi',
+  },
+
+  -- fidget
+  {"j-hui/fidget.nvim",},
 }
