c88f47dfc550c3c5443ffe8dbdc094be4b34df13
Flavio Moreira <fhnmor21@gmail.com>
2024-03-18 07:41:55 +1100

    replace fugitive for neogit

--- lua/plugins/coding/git.lua
+++ lua/plugins/coding/git.lua
@@ -1,10 +1,10 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    config = function ()
+    config = function()
       require("gitsigns").setup(
         {
-          signs = {
+          signs                        = {
             add          = { text = '│' },
             change       = { text = '│' },
             delete       = { text = '_' },
@@ -12,17 +12,17 @@ return {
             changedelete = { text = '~' },
             untracked    = { text = '┆' },
           },
-          signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-          numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
-          linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-          word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-          watch_gitdir = {
+          signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+          numhl                        = true, -- Toggle with `:Gitsigns toggle_numhl`
+          linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+          word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+          watch_gitdir                 = {
             follow_files = true
           },
-          auto_attach = true,
-          attach_to_untracked = false,
-          current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-          current_line_blame_opts = {
+          auto_attach                  = true,
+          attach_to_untracked          = false,
+          current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+          current_line_blame_opts      = {
             virt_text = true,
             virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
             delay = 1000,
@@ -30,11 +30,11 @@ return {
             virt_text_priority = 100,
           },
           current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-          sign_priority = 6,
-          update_debounce = 100,
-          status_formatter = nil, -- Use default
-          max_file_length = 40000, -- Disable if file is longer than this (in lines)
-          preview_config = {
+          sign_priority                = 6,
+          update_debounce              = 100,
+          status_formatter             = nil, -- Use default
+          max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+          preview_config               = {
             -- Options passed to nvim_open_win
             border = 'single',
             style = 'minimal',
@@ -42,7 +42,7 @@ return {
             row = 0,
             col = 1
           },
-          yadm = {
+          yadm                         = {
             enable = false
           },
         }
@@ -50,7 +50,21 @@ return {
     end,
   },
   {
-    "tpope/vim-fugitive",
-  },
-}
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
 
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+        keys = {
+      { "<leader>go", "<cmd> Neogit <CR>", desc = "Open Neogit" },
+      -- { "<leader>go", "function require('neogit').open() end", desc = "Open Neogit" },
+    },
+  },
+  -- {
+  --   "tpope/vim-fugitive",
+  -- },
+}
