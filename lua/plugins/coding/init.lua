f46e30c4a75043d517d7228b7fad3b67a991ab0d
FlavioM <fhnmor21@gmail.com>
2026-02-09 17:45:46 +1100

    fixing md and obsidian configs

--- lua/plugins/coding/init.lua
+++ lua/plugins/coding/init.lua
@@ -29,6 +29,17 @@ return {
   -- obsidian vault editor
   require("plugins.coding.obsidian"),
 
+  -- markdown
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
 --[[
 
   -- allow editing and integrate org mode files
--- lua/plugins/coding/obsidian.lua
+++ lua/plugins/coding/obsidian.lua
@@ -1,15 +1,15 @@
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = "markdown",
+  -- ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
+  event = {
   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
   --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
+    "BufReadPre ~/Vaults/**.md",
+    "BufNewFile ~/Vaults/**.md",
+  },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
@@ -28,7 +28,7 @@ return {
       },
       {
         name = "personal",
-        path = "~/Vaults/Personal",
+        path = "~/Vaults/Notes",
       },
       {
         name = "work",
